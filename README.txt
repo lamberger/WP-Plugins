6d8771b5a53188ce1cb02285984ca1e0d6da6db9
Patrik Lamberger <itutvecklare@gmail.com>
2012-01-07 19:44:09 +0100

    Add PL Admin login logo
    Add plugin and update read me

--- README.txt
+++ README.txt
@@ -3,6 +3,9 @@ Wordpress plugins
 1. PL Excerpt Control Version: 1.0
    A simple plugin that set the number of words in the excerpt and ends with a "Read more" link
 
+2. PL Admin Login Logo Version: 1.0
+   A simple plugin that let you change the logo in login admin view
+
 
 Author: Patrik Lamberger   
 Author URI: https://github.com/lamberger 
---
+++ customLoginLogo.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: PL Admin login logo
+Plugin URI: https://github.com/lamberger
+Description: Change the admin login logo
+Version: 1.0
+Author: Patrik Lamberger   
+Author URI: https://github.com/lamberger 
+License: GNU General Public License, version 2, GPL2
+*/  
+ 
+/* 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+*/   
+
+// Custom admin login logo. Place your image in the theme folder.You may need to change the path.
+function kupl_custom_login_logo() {
+	echo '<style type="text/css">
+			h1 a { background-image: url('.get_bloginfo('template_directory').'/img/logo.png) !important; }
+		  </style>';
+}
+add_action('login_head', 'kupl_custom_login_logo');
